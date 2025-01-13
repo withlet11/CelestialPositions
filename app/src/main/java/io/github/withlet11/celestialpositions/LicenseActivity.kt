8e9a8b450fc88f167351034f117862a9af194b29
withlet11 <withlet11@gmail.com>
2025-01-13 20:52:48 +0100

    Migrated LicenseActivity to Jetpack Compose, and removed some unused files.

--- app/src/main/java/io/github/withlet11/celestialpositions/LicenseActivity.kt
+++ app/src/main/java/io/github/withlet11/celestialpositions/LicenseActivity.kt
@@ -1,7 +1,7 @@
 /**
  * LicenseActivity.kt
  *
- * Copyright 2020 Yasuhiro Yamakawa <withlet11@gmail.com>
+ * Copyright 2020-2025 Yasuhiro Yamakawa <withlet11@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -22,21 +22,53 @@
 package io.github.withlet11.celestialpositions
 
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 class LicenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_license)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContent {
+            MaterialTheme(colorScheme = darkColorScheme()) {
+                LicenceText()
+            }
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) finish()
-        return true
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun LicenceText() {
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = getString(R.string.license)) },
+                navigationIcon = {
+                    IconButton(onClick = { finish() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                })
+        }) { innerPadding ->
+            Text(
+                text = getString(R.string.app_license),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
-
 }
